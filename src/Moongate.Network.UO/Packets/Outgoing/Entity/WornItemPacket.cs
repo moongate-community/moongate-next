8ae1f9c1624cd05b63101418c5bfd8ba46d9d1bd
Tom <squid@stormwind.it>
2026-06-16 10:49:11 +0200

    feat(network): add WornItem packet (0x2E) and visible-layers helper

---
+++ src/Moongate.Network.UO/Packets/Outgoing/Entity/WornItemPacket.cs
@@ -0,0 +1,48 @@
+using Moongate.Network.Spans;
+using Moongate.Network.UO.Attributes;
+using Moongate.Network.UO.Base;
+using Moongate.Network.UO.Types.Packets;
+using Moongate.UO.Data.Entities.Items;
+using Moongate.UO.Data.Entities.Mobiles;
+using Moongate.UO.Data.Types.Items;
+
+namespace Moongate.Network.UO.Packets.Outgoing.Entity;
+
+/// <summary>
+/// Outgoing "Worn Item" (0x2E): equips a single visible item on a mobile's figure/paperdoll.
+/// </summary>
+[PacketHandler(OpCodeValue, PacketSizing.Fixed, Length = LengthValue, Description = "Worn Item")]
+public class WornItemPacket : BaseGameNetworkPacket
+{
+    private const byte OpCodeValue = 0x2E;
+    private const int LengthValue = 15;
+
+    public MobileEntity Mobile { get; }
+    public ItemEntity Item { get; }
+    public ItemLayerType Layer { get; }
+
+    public WornItemPacket(MobileEntity mobile, ItemEntity item, ItemLayerType layer)
+        : base(OpCodeValue, LengthValue)
+    {
+        ArgumentNullException.ThrowIfNull(mobile);
+        ArgumentNullException.ThrowIfNull(item);
+
+        Mobile = mobile;
+        Item = item;
+        Layer = layer;
+    }
+
+    public override void Write(ref SpanWriter writer)
+    {
+        writer.Write(OpCode);
+        writer.Write(Item.Id.Value);
+        writer.Write((ushort)Item.ItemId);
+        writer.Write((byte)0);
+        writer.Write((byte)Layer);
+        writer.Write(Mobile.Id.Value);
+        writer.Write(Item.Hue.Value);
+    }
+
+    protected override bool ParsePayload(ref SpanReader reader)
+        => false;
+}
---
+++ src/Moongate.Server/Data/Internal/Packets/WornItemLayers.cs
@@ -0,0 +1,19 @@
+using Moongate.Core.Ids;
+using Moongate.UO.Data.Entities.Mobiles;
+using Moongate.UO.Data.Types.Items;
+
+namespace Moongate.Server.Data.Internal.Packets;
+
+/// <summary>
+/// Selects the equipped layers that should be sent to clients as visible worn items.
+/// </summary>
+public static class WornItemLayers
+{
+    public static IEnumerable<KeyValuePair<ItemLayerType, Serial>> VisibleEquipped(MobileEntity mobile)
+    {
+        ArgumentNullException.ThrowIfNull(mobile);
+
+        return mobile.EquippedItemIds.Where(static kv =>
+            kv.Key != ItemLayerType.Backpack && kv.Key != ItemLayerType.Bank);
+    }
+}
